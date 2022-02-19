c87b0b93e41cd625e4c3497cca30600a2f42b756
subhamsahu <sam766626@gmail.com>
2022-02-19 21:51:35 +0530

    UserAuthRestServices

---
+++ userservice/core/urls.py
@@ -0,0 +1,30 @@
+
+from django.urls import include, path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ShipBharat API",
+      default_version='v1',
+      description="Shipbharat User, Store APIs List",
+      terms_of_service="https://www.shipbharat.com/policies/terms/",
+      contact=openapi.Contact(email="contact@shipbharat.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
